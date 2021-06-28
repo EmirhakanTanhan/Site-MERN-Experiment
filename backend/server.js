1c26283197ca03319e868fcd6efedc7819ad9805
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2021-06-28 13:49:30 +0300

    imp

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import restaurants from "./api/restaurants.route.js";
+import restaurants from "./api/restaurants.route";
 
 const app = express();
 
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/restaurants", restaurants);
-app.use("*", (req, res) => res.status(404).json({error: "not found"}))
+app.use("*", (req, res) => res.status(404).json({error: "not found"}));
 
-export default app
+export default app;
